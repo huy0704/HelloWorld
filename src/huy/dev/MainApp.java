425d38c9b86f38bbd1c5bc0a436d58749ae2a5a0
huy <lmtctest01@gmail.com>
2021-10-04 16:32:36 +0700

    first commit

--- src/huy/dev/MainApp.java
+++ src/huy/dev/MainApp.java
@@ -4,12 +4,7 @@ public class MainApp {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello World!");
-       	
-        
-        // Thằng mẫn bị điên 
-        // thầng mẫn khồng
-        
+        System.out.println("Hello World!");   
 	}
 }
 
