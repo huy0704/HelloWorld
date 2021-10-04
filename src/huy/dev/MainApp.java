b02c26b957dd970ee281da40353158b69f0a619a
Huy <Huytruong113113@gmail.com>
2021-10-04 16:00:44 +0700

    first commit

---
+++ src/huy/dev/MainApp.java
@@ -0,0 +1,23 @@
+package huy.dev;
+
+public class MainApp {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World!");
+       	
+        
+        // Thằng mẫn bị điên 
+        // thầng mẫn khồng
+        
+	}
+}
+
+		
+	    
+	
+       
+  
+       
+
+   
